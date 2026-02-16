9989f3ff4391fe5d66474c61cf9d6e3865da582e
Abhishek Prajapati <abhishek.p04.15@gmail.com>
2026-02-16 07:33:56 +0530

    models created for the project

---
+++ src/main/java/com/example/fitness/model/ActivityType.java
@@ -0,0 +1,14 @@
+package com.example.fitness.model;
+
+public enum ActivityType {
+    Running,
+    Walking,
+    Cycling,
+    Swimming,
+    Weight_training,
+    Yoga,
+    Hit,
+    Cardio,
+    Streching,
+    Other
+}
